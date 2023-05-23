01c985752d6aeff45a937261bf748c41e7357ce2
nic1k <nic1k.88@gmail.com>
2023-05-23 08:43:49 +0200

    Init system second

---
+++ bedrock/web/app/themes/byte-bible/app/Fields/CheetSheet.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Fields;
+
+use Log1x\AcfComposer\Field;
+use StoutLogic\AcfBuilder\FieldNameCollisionException;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+class CheetSheet extends Field
+{
+    /**
+     * The field group.
+     *
+     * @return array
+     * @throws FieldNameCollisionException
+     */
+    public function fields()
+    {
+
+        $fieldChaptersArg = [
+            'label' => 'Chapters',
+            'layout' => 'block'
+        ];
+
+        $fieldCheetsArg = [
+            'label' => 'Cheets'
+        ];
+
+        $cheetSheet = new FieldsBuilder('cheet_sheet');
+
+        $cheetSheet
+            ->setLocation('post_type', '==', 'cheetsheet');
+
+        $cheetSheet
+            ->addRepeater('chapters', $fieldChaptersArg)
+                ->addText('heading')
+                ->addRepeater('cheets', $fieldCheetsArg)
+                    ->addText('description')
+                    ->addTextarea('code')
+                ->endRepeater()
+            ->endRepeater();
+
+        return $cheetSheet->build();
+    }
+}
